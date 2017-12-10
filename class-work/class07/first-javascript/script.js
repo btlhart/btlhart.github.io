9481d1a82c5fbec229795e7acccd845fe5427497
btlhart <btlhart@gmail.com>
2017-12-10 13:39:03 -0500

    Finished Startup Matchmaker HW

---
+++ class-work/class07/first-javascript/script.js
@@ -0,0 +1,17 @@
+var product = 10 * 10;
+var quotient = 100 / 10;
+var difference = 50 - 17;
+var sum = 50 + 17;
+var name = 'Ben' + ' Hart';
+
+console.log(product + sum);
+
+function onra () {
+    var city = 'Saigon';
+
+    console.log(`Last Tango In ${city}`);
+}
+
+console.log(document.getElementById('hello'));
+
+hello.style.color = 'tomato';
