0378ffdc8a048bbcde498572ce53ef7401a0cdff
btlhart <btlhart@gmail.com>
2017-12-18 20:00:48 -0500

    jQuery City solution

--- class-work/class10/jquery-city/js/app.js
+++ class-work/class10/jquery-city/js/app.js
@@ -1,2 +1,19 @@
 // 1. Create .click() handlers for each of the thumbnails: #first, #second, #third, #fourth
+$('#first').click(function(){
+    $('#bigimage').attr('src', 'img/1.jpg')
+})
+
+$('#second').click(function(){
+        $('#bigimage').attr('src', 'img/2.jpg')
+})
+
+$('#third').click(function(){
+    $('#bigimage').attr('src', 'img/3.jpg')
+})
+
+$('#fourth').click(function(){
+    $('#bigimage').attr('src', 'img/4.jpg')
+})
+
+
 // 2. Use .attr() to change the `src` attribute of #bigimage to correspond to image that was clicked
