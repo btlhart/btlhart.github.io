556486b54dfdf908037be199ac167aab1fe2b9d4
btlhart <btlhart@gmail.com>
2018-01-08 21:29:05 -0500

    finished list-maker and moved lab to hw folder

--- homework/list-maker/js/app.js
+++ homework/list-maker/js/app.js
@@ -12,6 +12,13 @@
 
 $(document).ready(function () {
 
+		$('#clickme').click(function(){
+			$('#list').append('<li>' + $('#item').val());
+			$('#item').val('');
+		})
+
+
+
 	// 1. Attach a .click() event to #clickme; attach an anonymous function to the .click()
 	// 1A. This anonymous function should create a variable, newItem equal to the value of #item
 	// 1B. This anonymous function should call a separate function, appendItem, and pass it the variable newItem
