4c411af41ecee79738a6a6b663a267b6bbe15847
Ben Hart <btlhart@gmail.com>
2018-01-22 22:38:43 -0500

    class 16

--- class-work/class16/animated-sticky-nav/js/app.js
+++ class-work/class16/animated-sticky-nav/js/app.js
@@ -1,2 +1,14 @@
 $(document).ready(function () {
+
+    $(window).scroll(function(){
+        console.log($(window).scrollTop());
+
+        if ($(window).scrollTop() > 150) {
+            $('header').css('font-size', '20px');
+        } else {
+            $('header').css('font-size', '75px');
+        }
+        // It's better IRL to assign a class vs. write css in your javascript
+    })
+
 })
